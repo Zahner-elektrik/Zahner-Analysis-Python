b61df6f434b7016e783db7ee4f42092ecc912573
Maximilian Krapp <maximilian.krapp@zahner.de>
2022-08-22 08:26:37 +0200

    Publish package version 1.0.1
    Merged pull request which added a missing __ini__ file.
    #publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="zahner_analysis",
-    version="1.0.0",
+    version="1.0.1",
     author="Maximilian Krapp",
     author_email="maximilian.krapp@zahner.de",
     description="Python package for the analysis of electrochemical impedance spectra.",
